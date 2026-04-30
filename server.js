1f41fc5daa9a6adfb9377c28d086da9a7ed725fe
Subash Pandi <subashpandiofficial@gmail.com>
2026-04-30 16:42:20 +0530

    Enable CORS for Vercel frontend

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const express = require("express");
+const cors = require("cors");
 const { MongoClient } = require("mongodb");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
